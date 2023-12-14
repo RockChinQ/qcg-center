55e0e31a6994be0a78395ff730cb8dec6901f96e
RockChinQ <1010553892@qq.com>
2023-12-14 22:18:04 +0800

    fix: 修复时区错误

--- src/util/time.go
+++ src/util/time.go
@@ -3,9 +3,19 @@ package util
 import "time"
 
 func GetCSTTime() time.Time {
-	return time.Now().UTC().Add(time.Hour * 8)
+	utcTime := time.Now().UTC()
+
+	cstTime := time.Date(utcTime.Year(), utcTime.Month(), utcTime.Day(), utcTime.Hour(), utcTime.Minute(), utcTime.Second(), utcTime.Nanosecond(), GetCSTTimeLocation())
+
+	return cstTime
 }
 
 func GetCSTTimeLocation() *time.Location {
 	return time.FixedZone("CST", 8*3600)
+}
+
+func PrintTime(t time.Time) {
+	println(t.Format("2006-01-02 15:04:05"))
+	// 打印时区
+	println(t.Location().String())
 }
